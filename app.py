1d55877f56ec6aa396ef5fd25f74b77dfa140800
kajarenc <kajarenc@gmail.com>
2021-08-05 03:33:20 +0400

    minor fixes

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import streamlit as st
-st.title("🥑 Avocado Analytics!")
 
+with st.echo():
+    st.title("Download button filepath experiment")
 
-def generate_file_name(region, avocado_type, start_date, end_date):
-    return f"avocado_{region}_{avocado_type}_{str(start_date)}_{str(end_date)}.csv"
+    x = st.slider("label for slider", 0, 100, 28)
+    st.write(x)
 
+    st.download_button(
+        "39 MB file",
+        data=None,
+        filepath="lalala.csv"
+    )
 
-x = st.slider("label for slider", 0, 100, 28)
-st.write(x)
-
-st.download_button(
-    "LALALEND",
-    data=None,
-    filepath="lalala.csv"
-)
-
-st.download_button(
-    "TADADA",
-    data=None,
-    filepath="mmm/international-migration.csv"
-)
+    st.download_button(
+        "39 mb file nested in directory",
+        data=None,
+        filepath="mmm/international-migration.csv"
+    )
