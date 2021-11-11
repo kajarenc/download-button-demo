8b4e6f81942d811838941d876993ab00f422b74e
kajarenc <kajarenc@gmail.com>
2021-11-11 19:33:00 +0400

    Add widgets inside forms

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ image_file = st.camera_image_input()
 if image_file:
     st.image(image_file.getvalue())
 
+with st.form(key="key1"):
+    x = st.slider("Slider X:", 0, 100, 33)
+    image_file_inside_form = st.camera_image_input()
+    form_submitted = st.form_submit_button("Submit")
+
+if form_submitted:
+    st.write(f"Slider value X=: {x}")
+    st.image(image_file_inside_form.getvalue())
+
+with st.form(key="key2", clear_on_submit=True):
+    y = st.slider("Slider Y:", 0, 100, 42)
+    image_file_inside_clear_on_submit_form = st.camera_image_input()
+    clear_on_submit_form_submitted = st.form_submit_button("Submit, clear form on submit")
+
+if clear_on_submit_form_submitted:
+    st.write(f"Slider value Y=: {y}")
+    st.image(image_file_inside_clear_on_submit_form.getvalue())
+
